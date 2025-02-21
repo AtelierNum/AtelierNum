dcda105f63d1e676605f129d861ab92a9ca9ce49
valentin dupas <valentin.lc.dupas@gmail.com>
2025-02-21 21:22:23 +0100

    include the quests.html page into the build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         mentions: resolve(__dirname, "mentions.html"),
+        quests: resolve(__dirname, "quests.html"),
       },
     },
   },
